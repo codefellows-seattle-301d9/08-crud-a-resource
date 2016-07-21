ce97bafb1392e03b41a4a5d1ee35f59b95fc6263
James <canning.jimmy@gmail.com>
2016-07-20 18:06:05 -0700

    Compleated all todo items

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -20,7 +20,7 @@
 
   // Set up a DB table for articles.
   Article.createTable = function() {
-    var inputString = 'CREATE TABLE IF NOT EXISTS articles_table (title VARCHAR, category VARCHAR, author VARCHAR, author_url VARCHAR, published_on DATE, body TEXT);';
+    var inputString = 'CREATE TABLE IF NOT EXISTS articles_table (id INTEGER PRIMARY KEY, title VARCHAR, category VARCHAR, author VARCHAR, authorUrl VARCHAR, publishedOn DATE, body TEXT);';
 
     webDB.execute(
       [
@@ -31,7 +31,7 @@
           , // TODO DONE: What SQL command do we run here inside these quotes? - createTable (use wrap) apply callback fn
 
       function() {
-        console.log('Successfully set up the articles table.');
+        console.log('createTable ran.');
       }
     );
   };
@@ -54,7 +54,7 @@
         Article.loadAll(rows);
         nextFunction();
       }
-        /* TODO:
+        /* TODO DONE:
            1 - Use Article.loadAll to instanitate these rows,
            2 - Pass control to the view by invoking the next function that
                 was passed in to Article.fetchAll */
@@ -64,14 +64,14 @@
           // Save each article from this JSON file, so we don't need to request it next time:
           responseData.forEach(function(obj) {
             var article = new Article(obj); // This will instantiate an article instance based on each article object from our JSON.
-            /* TODO:
+            /* TODO DONE:
                1 - 'insert' the newly-instantiated article in the DB:
                 (hint: what can we call on this article instance?). */
             article.insertRecord();
           });
           // Now get ALL the records out the DB, with their database IDs:
           webDB.execute([{'sql': 'SELECT * FROM articles_table'}], function(rows) { // TODO: select our now full table
-            // TODO:
+            // TODO DONE:
             // 1 - Use Article.loadAll to generate our rows,
             // 2 - Pass control to the view by calling the next function that was passed in to Article.fetchAll
             Article.loadAll(rows);
@@ -87,10 +87,10 @@
     webDB.execute(
       [
         {
-          // TODO: Insert an article instance into the database:
+          // TODO DONE: Insert an article instance into the database:
           // NOTE: this method will be called elsewhere after we retrieve our JSON
-          'sql': 'INSERT INTO articles_table (title, category, author, author_url, published_on, body) VALUES (?, ?, ?, ?, ?, ?);', // <----- complete our SQL command here, inside the quotes.
-          'data': [this.title, this.category, this.author, this.author_url, this.published_on, this.body]
+          'sql': 'INSERT INTO articles_table (title, category, author, authorUrl, publishedOn, body) VALUES (?, ?, ?, ?, ?, ?);', // <----- complete our SQL command here, inside the quotes.
+          'data': [this.title, this.category, this.author, this.authorUrl, this.publishedOn, this.body]
         }
       ]
     );
@@ -100,10 +100,7 @@
     webDB.execute(
       [
         {
-          // TODO: Delete an article instance from the database based on its id:
-          /* Note: this is an advanced admin option, so you will need to test
-              out an individual query in the console */
-          'sql': '', // <--- complete the command here, inside the quotes;
+          'sql': 'DELETE FROM articles_table WHERE rowid = ?;', 
           'data': [this.id]
         }
       ]
@@ -111,9 +108,12 @@
   };
 
   Article.truncateTable = function() {
-    webDB.execute(
-      // TODO: Use correct SQL syntax to delete all records from the articles table.
-      'DELETE ...;' // <----finish the command here, inside the quotes.
+    webDB.execute( 
+      [
+        { 
+          'sql': 'DELETE FROM articles_table;' 
+        }
+      ]
     );
   };
 
@@ -155,7 +155,7 @@
     });
   };
 
-// TODO: ensure that our table has been setup.
+// TODO DONE: ensure that our table has been setup.
   Article.createTable();
 
   module.Article = Article;
